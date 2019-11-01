2bb60effb268e3203f4e795095196c4883b39e42
VK <vitaliy.krynytskyy@gmail.com>
2019-11-01 13:35:59 +0000

    Added a Vote model

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -8,3 +8,10 @@ class Link(models.Model):
     posted_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
     )
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    link = models.ForeignKey(
+        'links.Link', related_name='votes', on_delete=models.CASCADE)
