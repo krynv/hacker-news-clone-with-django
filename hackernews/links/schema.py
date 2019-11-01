ee046590fd10795294e03ef11f7fda94360c2f87
VK <vitaliy.krynytskyy@gmail.com>
2019-11-01 14:28:57 +0000

    Added the ability to search for Links

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
-
+from django.db.models import Q
 
 from .models import Link
 from users.schema import UserType
@@ -43,10 +43,17 @@ class CreateVote(graphene.Mutation):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_links(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
